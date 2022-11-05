81967fea3f412a45d6275abddc71be5571079bb6
Thanatad Songpetchmongkol <thanatad@gmail.com>
2022-11-05 17:13:34 +0000

    :zap: Finish with dashboard route.

--- backend/app/controllers/dashboard.controllers.js
+++ backend/app/controllers/dashboard.controllers.js
@@ -1,12 +1,38 @@
-// const db = require("../models");
-// const User = db.users;
-const userRepository = require("../repository/user.repository");
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const UCL_URL_TIMETABLE = 'https://uclapi.com/timetable/personal';
 
 async function getAll(req, res) {
-    console.log("userRepository: getAll()");
+    try {
+        console.log("dashboard controller: getAll()");
+        const TOKEN = req.body.token;
 
-    // const TOKEN = req.cookie.TOKEN;
+        /* Getting timetable */
+        // let CURRENT_DATE = new Date()
+        // CURRENT_DATE.toISOString().split('T')[0]
+        let CURRENT_DATE = "2022-11-03"
 
+        const response = await axios.get(`${UCL_URL_TIMETABLE}?client_secret=${process.env.CLIENT_SECRET}&date=${CURRENT_DATE}&token=${TOKEN}`);
+
+        // const response = await axios.get("https://uclapi.com/timetable/personal?client_secret=0a2eb6400a02cb27c3fa05d2fcae6caaecd03da0d37a3bc3bdb682b8c8264abb&date=2022-09-28&token=uclapi-user-bc81a10a069e589-e6614ad9d9956c0-cf4b32e70097f92-102349296dc1413");
+
+        const timetable = response.data.timetable;
+
+        res.status(200).send({
+            student: {
+                name: req.fullName,
+                degree: "MSc Software Systems Engineering",
+            },
+            timetable: timetable
+        });
+    } catch (error) {
+        // console.log(error)
+        res.status(500).send({
+            message: error,
+        })
+    } 
 }
 
 
--- backend/app/middleware/user.middleware.js
+++ backend/app/middleware/user.middleware.js
@@ -5,11 +5,9 @@ dotenv.config();
 const UCL_URL = 'https://uclapi.com/oauth/user/data';
 
 async function verifyToken(req, res, next) {
-    const TOKEN = req.body.token;
-
     console.log("middleware: verifyToken()");
-    console.log("TOKEN: ", TOKEN);
 
+    const TOKEN = req.body.token;
     if (typeof TOKEN === 'undefined') {
         return res.status(401).send({
             message: "No token provided."
@@ -17,9 +15,14 @@ async function verifyToken(req, res, next) {
     }
 
     const response = await axios.get(`${UCL_URL}?client_secret=${process.env.CLIENT_SECRET}&token=${TOKEN}`);
-    console.log(response.status);
-    console.log(response.data);
-    return;
+    if (response.status !== 200) {
+        return res.status(500).send({
+            message: "Error with provided token."
+        });
+    }
+
+    req.fullName = response.data.full_name
+    next();
 }
 
 module.exports = {
