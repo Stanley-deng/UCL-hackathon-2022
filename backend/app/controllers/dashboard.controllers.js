9fbe73201705968768b861c6c8cb6d8675294c4b
Thanatad Songpetchmongkol <thanatad@gmail.com>
2022-11-06 12:49:43 +0000

    :bug: Hot fix dashboard route.

--- backend/app/controllers/dashboard.controllers.js
+++ backend/app/controllers/dashboard.controllers.js
@@ -7,37 +7,37 @@ const UCL_URL_STAFF = 'https://uclapi.com/search/people';
 //dummy search value
 const dummySearchValue = 'jen';
 
+const userRepository = require("../repository/user.repository");
+
 async function getAll(req, res) {
     try {
         console.log("dashboard controller: getAll()");
         const TOKEN = req.body.token;
 
         /* Getting timetable */
-        let CURRENT_DATE = new Date()
-        CURRENT_DATE.toISOString().split('T')[0]
-        //let CURRENT_DATE = "2022-11-03"
+        // let CURRENT_DATE = new Date()
+        // CURRENT_DATE.toISOString().split('T')[0]
+        let CURRENT_DATE = "2022-11-03"
 
 
         const response_timetable = await axios.get(`${UCL_URL_TIMETABLE}?client_secret=${process.env.CLIENT_SECRET}&date=${CURRENT_DATE}&token=${TOKEN}`);
         const response_staff = await axios.get(`${UCL_URL_STAFF}?query=${dummySearchValue}&client_secret=${process.env.CLIENT_SECRET}&token=${TOKEN}`);
 
         const timetable = response_timetable.data.timetable;
-        //const staff = response_staff.data.staff;
 
-        //console.log(response_staff);
+        const response_user = await userRepository.findOne(req.email);
+        const note = response_user.note;
 
         res.status(200).send({
             student: {
                 name: req.fullName,
                 degree: "MSc Software Systems Engineering",
             },
-            timetable: timetable[CURRENT_DATE],
-            note:{
-                name: findOne(req.email)
-            }
+            timetable: timetable[CURRENT_DATE] ? timetable[CURRENT_DATE] : [],
+            note: note,
         });
     } catch (error) {
-        // console.log(error)
+        console.log(error)
         res.status(500).send({
             message: error,
         })
