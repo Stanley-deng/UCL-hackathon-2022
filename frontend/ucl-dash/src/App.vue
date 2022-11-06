123fe2218bf82b4df1c824a19aef9572920f1204
Danny <danielswarup1@gmail.com>
2022-11-06 10:50:28 +0000

    bug: fix search pixel gap

--- frontend/ucl-dash/src/App.vue
+++ frontend/ucl-dash/src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <RouterView />
 </template>
 
